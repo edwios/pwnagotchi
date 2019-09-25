b9ecb09d10089ab2aa47cd39719cd42f4c6fa4fd
Edwios <edwintam@iostation.com>
2019-09-25 14:34:24 +0800

    Modified to use with old 2.13in WaveShare eInk HAT

--- sdcard/rootfs/root/pwnagotchi/scripts/pwnagotchi/ui/waveshare.py
+++ sdcard/rootfs/root/pwnagotchi/scripts/pwnagotchi/ui/waveshare.py
@@ -48,7 +48,7 @@
 import time
 import spidev
 import RPi.GPIO as GPIO
-from PIL import Image
+from PIL import Image, ImageDraw
 
 # Pin definition
 RST_PIN = 17
@@ -89,9 +89,31 @@ def module_init():
 
 
 # Display resolution
-EPD_WIDTH = 122
+EPD_WIDTH = 128
 EPD_HEIGHT = 250
 
+# EPD2IN13 commands
+DRIVER_OUTPUT_CONTROL                       = 0x01
+BOOSTER_SOFT_START_CONTROL                  = 0x0C
+GATE_SCAN_START_POSITION                    = 0x0F
+DEEP_SLEEP_MODE                             = 0x10
+DATA_ENTRY_MODE_SETTING                     = 0x11
+SW_RESET                                    = 0x12
+TEMPERATURE_SENSOR_CONTROL                  = 0x1A
+MASTER_ACTIVATION                           = 0x20
+DISPLAY_UPDATE_CONTROL_1                    = 0x21
+DISPLAY_UPDATE_CONTROL_2                    = 0x22
+WRITE_RAM                                   = 0x24
+WRITE_VCOM_REGISTER                         = 0x2C
+WRITE_LUT_REGISTER                          = 0x32
+SET_DUMMY_LINE_PERIOD                       = 0x3A
+SET_GATE_TIME                               = 0x3B
+BORDER_WAVEFORM_CONTROL                     = 0x3C
+SET_RAM_X_ADDRESS_START_END_POSITION        = 0x44
+SET_RAM_Y_ADDRESS_START_END_POSITION        = 0x45
+SET_RAM_X_ADDRESS_COUNTER                   = 0x4E
+SET_RAM_Y_ADDRESS_COUNTER                   = 0x4F
+TERMINATE_FRAME_READ_WRITE                  = 0xFF
 
 class EPD:
     def __init__(self):
@@ -103,40 +125,19 @@ class EPD:
 
     FULL_UPDATE = 0
     PART_UPDATE = 1
+
     lut_full_update = [
-        0x80, 0x60, 0x40, 0x00, 0x00, 0x00, 0x00,  # LUT0: BB:     VS 0 ~7
-        0x10, 0x60, 0x20, 0x00, 0x00, 0x00, 0x00,  # LUT1: BW:     VS 0 ~7
-        0x80, 0x60, 0x40, 0x00, 0x00, 0x00, 0x00,  # LUT2: WB:     VS 0 ~7
-        0x10, 0x60, 0x20, 0x00, 0x00, 0x00, 0x00,  # LUT3: WW:     VS 0 ~7
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  # LUT4: VCOM:   VS 0 ~7
-
-        0x03, 0x03, 0x00, 0x00, 0x02,  # TP0 A~D RP0
-        0x09, 0x09, 0x00, 0x00, 0x02,  # TP1 A~D RP1
-        0x03, 0x03, 0x00, 0x00, 0x02,  # TP2 A~D RP2
-        0x00, 0x00, 0x00, 0x00, 0x00,  # TP3 A~D RP3
-        0x00, 0x00, 0x00, 0x00, 0x00,  # TP4 A~D RP4
-        0x00, 0x00, 0x00, 0x00, 0x00,  # TP5 A~D RP5
-        0x00, 0x00, 0x00, 0x00, 0x00,  # TP6 A~D RP6
-
-        0x15, 0x41, 0xA8, 0x32, 0x30, 0x0A,
+        0x22, 0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x11,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E, 0x1E,
+        0x01, 0x00, 0x00, 0x00, 0x00, 0x00
     ]
 
-    lut_partial_update = [  # 20 bytes
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  # LUT0: BB:     VS 0 ~7
-        0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  # LUT1: BW:     VS 0 ~7
-        0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  # LUT2: WB:     VS 0 ~7
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  # LUT3: WW:     VS 0 ~7
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  # LUT4: VCOM:   VS 0 ~7
-
-        0x0A, 0x00, 0x00, 0x00, 0x00,  # TP0 A~D RP0
-        0x00, 0x00, 0x00, 0x00, 0x00,  # TP1 A~D RP1
-        0x00, 0x00, 0x00, 0x00, 0x00,  # TP2 A~D RP2
-        0x00, 0x00, 0x00, 0x00, 0x00,  # TP3 A~D RP3
-        0x00, 0x00, 0x00, 0x00, 0x00,  # TP4 A~D RP4
-        0x00, 0x00, 0x00, 0x00, 0x00,  # TP5 A~D RP5
-        0x00, 0x00, 0x00, 0x00, 0x00,  # TP6 A~D RP6
-
-        0x15, 0x41, 0xA8, 0x32, 0x30, 0x0A,
+    lut_partial_update  = [
+        0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x0F, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
     ]
 
     # Hardware reset
@@ -166,29 +167,42 @@ class EPD:
         self.send_command(0x20)
         self.wait_until_idle()
 
+##
+ #  @brief: set the look-up table register
+ ##
+    def set_lut(self, lut):
+        self.lut = lut
+        self.send_command(WRITE_LUT_REGISTER)
+        # the length of look-up table is 30 bytes
+        for i in range(0, len(lut)):
+            self.send_data(self.lut[i])
+
     def init(self, update):
         if (module_init() != 0):
             return -1
         # EPD hardware init start
         self.reset()
+        self.wait_until_idle()
         if (update == self.FULL_UPDATE):
-            self.wait_until_idle()
+            self.lut = self.lut_full_update
             self.send_command(0x12)  # soft reset
             self.wait_until_idle()
-
-            self.send_command(0x74)  # set analog block control
-            self.send_data(0x54)
-            self.send_command(0x7E)  # set digital block control
-            self.send_data(0x3B)
-
-            self.send_command(0x01)  # Driver output control
-            self.send_data(0xF9)
-            self.send_data(0x00)
-            self.send_data(0x00)
-
-            self.send_command(0x11)  # data entry mode
-            self.send_data(0x01)
-
+            self.send_command(DRIVER_OUTPUT_CONTROL)
+            self.send_data((EPD_HEIGHT - 1) & 0xFF)
+            self.send_data(((EPD_HEIGHT - 1) >> 8) & 0xFF)
+            self.send_data(0x00)                     # GD = 0 SM = 0 TB = 0
+            self.send_command(BOOSTER_SOFT_START_CONTROL)
+            self.send_data(0xD7)
+            self.send_data(0xD6)
+            self.send_data(0x9D)
+#            self.send_command(WRITE_VCOM_REGISTER)
+#            self.send_data(0x9B)                     # VCOM 7C
+            self.send_command(SET_DUMMY_LINE_PERIOD)
+            self.send_data(0x1A)                     # 4 dummy lines per gate
+            self.send_command(SET_GATE_TIME)
+            self.send_data(0x08)                     # 2us per line
+            self.send_command(DATA_ENTRY_MODE_SETTING)
+            self.send_data(0x03)                     # X increment Y increment
             self.send_command(0x44)  # set Ram-X address start//end position
             self.send_data(0x00)
             self.send_data(0x0F)  # 0x0C-->(15+1)*8=128
@@ -204,24 +218,7 @@ class EPD:
 
             self.send_command(0x2C)  # VCOM Voltage
             self.send_data(0x55)  #
-
-            self.send_command(0x03)
-            self.send_data(self.lut_full_update[70])
-
-            self.send_command(0x04)  #
-            self.send_data(self.lut_full_update[71])
-            self.send_data(self.lut_full_update[72])
-            self.send_data(self.lut_full_update[73])
-
-            self.send_command(0x3A)  # Dummy Line
-            self.send_data(self.lut_full_update[74])
-            self.send_command(0x3B)  # Gate time
-            self.send_data(self.lut_full_update[75])
-
-            self.send_command(0x32)
-            for count in range(70):
-                self.send_data(self.lut_full_update[count])
-
+            self.set_lut(self.lut)
             self.send_command(0x4E)  # set RAM x address count to 0
             self.send_data(0x00)
             self.send_command(0x4F)  # set RAM y address count to 0X127
@@ -229,32 +226,46 @@ class EPD:
             self.send_data(0x00)
             self.wait_until_idle()
         else:
+            self.lut = self.lut_partial_update
             self.send_command(0x2C)  # VCOM Voltage
             self.send_data(0x26)
-
             self.wait_until_idle()
-
-            self.send_command(0x32)
-            for count in range(70):
-                self.send_data(self.lut_partial_update[count])
-
-            self.send_command(0x37)
-            self.send_data(0x00)
-            self.send_data(0x00)
-            self.send_data(0x00)
-            self.send_data(0x00)
-            self.send_data(0x40)
-            self.send_data(0x00)
-            self.send_data(0x00)
-
+            self.set_lut(self.lut)
             self.send_command(0x22)
             self.send_data(0xC0)
             self.send_command(0x20)
             self.wait_until_idle()
-
             self.send_command(0x3C)  # BorderWavefrom
             self.send_data(0x01)
+
+
         return 0
+
+##
+ #  @brief: specify the memory area for data R/W
+ ##
+    def set_memory_area(self, x_start, y_start, x_end, y_end):
+        self.send_command(SET_RAM_X_ADDRESS_START_END_POSITION)
+        # x point must be the multiple of 8 or the last 3 bits will be ignored
+        self.send_data((x_start >> 3) & 0xFF)
+        self.send_data((x_end >> 3) & 0xFF)
+        self.send_command(SET_RAM_Y_ADDRESS_START_END_POSITION)
+        self.send_data(y_start & 0xFF)
+        self.send_data((y_start >> 8) & 0xFF)
+        self.send_data(y_end & 0xFF)
+        self.send_data((y_end >> 8) & 0xFF)
+
+##
+ #  @brief: specify the start point for data R/W
+ ##
+    def set_memory_pointer(self, x, y):
+        self.send_command(SET_RAM_X_ADDRESS_COUNTER)
+        # x point must be the multiple of 8 or the last 3 bits will be ignored
+        self.send_data((x >> 3) & 0xFF)
+        self.send_command(SET_RAM_Y_ADDRESS_COUNTER)
+        self.send_data(y & 0xFF)
+        self.send_data((y >> 8) & 0xFF)
+        self.wait_until_idle()
 
     def getbuffer(self, image):
         if self.width % 8 == 0:
@@ -285,32 +296,63 @@ class EPD:
                         buf[newx // 8 + newy * linewidth] &= ~(0x80 >> (y % 8))
         return buf
 
-    def display(self, image):
-        if self.width % 8 == 0:
-            linewidth = self.width // 8
+##
+ #  @brief: put an image to the frame memory.
+ #          this won't update the display.
+ ##
+    def set_frame_memory(self, image, x, y):
+        if (image == None or x < 0 or y < 0):
+            return
+        image_monocolor = image.convert('1')
+        image_width, image_height  = image_monocolor.size
+        # x point must be the multiple of 8 or the last 3 bits will be ignored
+        x = x & 0xF8
+        image_width = image_width & 0xF8
+        if (x + image_width >= self.width):
+            x_end = self.width - 1
         else:
-            linewidth = self.width // 8 + 1
+            x_end = x + image_width - 1
+        if (y + image_height >= self.height):
+            y_end = self.height - 1
+        else:
+            y_end = y + image_height - 1
+        self.set_memory_area(x, y, x_end, y_end)
+        # send the image data
+        pixels = image_monocolor.load()
+        byte_to_send = 0x00
+        for j in range(y, y_end + 1):
+            self.set_memory_pointer(x, j)
+            self.send_command(WRITE_RAM)
+            # 1 byte = 8 pixels, steps of i = 8
+            for i in range(x, x_end + 1):
+                # Set the bits for the column of pixels at the current position.
+                if pixels[i - x, j - y] != 0:
+                    byte_to_send |= 0x80 >> (i % 8)
+                if (i % 8 == 7):
+                    self.send_data(byte_to_send)
+                    byte_to_send = 0x00
 
-        self.send_command(0x24)
-        for j in range(0, self.height):
-            for i in range(0, linewidth):
-                self.send_data(image[i + j * linewidth])
+##
+ #  @brief: clear the frame memory with the specified color.
+ #          this won't update the display.
+ ##
+    def clear_frame_memory(self, color):
+        self.set_memory_area(0, 0, self.width - 1, self.height - 1)
+        self.set_memory_pointer(0, 0)
+        self.send_command(WRITE_RAM)
+        # send the color data
+        for i in range(0, int(self.width / 8 * self.height)):
+            self.send_data(color)
+
+
+    def display(self, image):
+        self.clear_frame_memory(0xFF)
+        self.set_frame_memory(image, 0, 0)
         self.TurnOnDisplay()
 
     def displayPartial(self, image):
-        if self.width % 8 == 0:
-            linewidth = self.width // 8
-        else:
-            linewidth = self.width // 8 + 1
-
-        self.send_command(0x24)
-        for j in range(0, self.height):
-            for i in range(0, linewidth):
-                self.send_data(image[i + j * linewidth])
-        self.send_command(0x26)
-        for j in range(0, self.height):
-            for i in range(0, linewidth):
-                self.send_data(~image[i + j * linewidth])
+        self.clear_frame_memory(0xFF)
+        self.set_frame_memory(image, 0, 0)
         self.TurnOnDisplay()
 
     def Clear(self, color):
@@ -325,6 +367,28 @@ class EPD:
             for i in range(0, linewidth):
                 self.send_data(color)
         self.TurnOnDisplay()
+
+    def Wipe(self, color):
+        step = 16
+        x = 0
+        f = True 
+        while(f):
+            bimage = Image.new('1', (self.width, self.height), 255)  # 255: clear the frame
+            bdraw = ImageDraw.Draw(bimage)
+            bdraw.rectangle((x, 0, step+x-1, self.height-1), fill = 0)
+            self.clear_frame_memory(0xFF)
+            self.set_frame_memory(bimage, 0, 0)
+            self.TurnOnDisplay()
+            bimage = Image.new('1', (self.width, self.height), 255)  # 255: clear the frame
+            bdraw = ImageDraw.Draw(bimage)
+            bdraw.rectangle((x, 0, step+x-1, self.height-1), fill = 0)
+            self.clear_frame_memory(0xFF)
+            self.set_frame_memory(bimage, 0, 0)
+            self.TurnOnDisplay()
+            x += step
+            f = (x <= self.width)
+
+
 
     def sleep(self):
         self.send_command(0x22)  # POWER OFF
